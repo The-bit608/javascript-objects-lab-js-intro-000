35651c2bbcddb6135dd633f60f9108ee6e1494c6
Grant Stafford-Renard <gstaffordrenard@gmail.com>
2020-08-13 15:39:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function updateObjectWithKeyAndValue(object, key, value) {
-Object.assin({}, object, { [key]: value})
+Object.assign({}, object, { [key]: value})
 return object
 }
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
